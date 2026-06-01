14fcb744fb45393bc7fb7f819b4731ba46b2f741
sonu alam <sonu.alam@rekise.com>
2026-06-01 17:14:19 +0530

    Add users table migration, pooled DB connection, startup check and DB healthcheck

--- services/auth/src/index.js
+++ services/auth/src/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import {query} from "./db.js"
 
 const app = express();
 const port = process.env.AUTH_PORT || 3001;
 
+async function checkDbConnection(){
+    try {
+        await query("SELECT 1");
+        console.log("DB Connection ok")
+    } catch (error) {
+        console.error("DB Connection FAILED: ", error.message)
+        process.exit(1)
+        
+    }
+}
 
 app.get("/health", (req, res) => {
     res.status(200).json({
@@ -11,7 +22,7 @@ app.get("/health", (req, res) => {
 })
 
 
-
+await checkDbConnection();
 app.listen(port, () => {
     console.log(`Server is listening to port http://localhost:${port}`)
 })
